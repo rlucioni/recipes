c22f9bb2d4799e92410ba1b1b58c502a9be62bdd
Renzo Lucioni <renzolucioni@gmail.com>
2025-02-23 14:00:01 -0500

    use all non-archived recipes

--- chefbot/app.py
+++ chefbot/app.py
@@ -4,6 +4,7 @@ import os
 import random
 import re
 from logging.config import dictConfig
+from pathlib import Path
 
 from dotenv import load_dotenv
 from flask import Flask, request
@@ -43,7 +44,6 @@ logger = logging.getLogger(__name__)
 
 load_dotenv('.env.private')
 
-# TODO: slight preference for vegetarian options?
 DEVELOPER_MESSAGE_TEMPLATE = """You're a private chef named chefbot. The couple you work for has given you examples of their favorite recipes as Markdown below. Help them meal plan, either by using these recipes or thinking of new ones that you think they'd like based on the given examples. Be concise and include no superfluous details. When sharing a recipe, use the format of the included Markdown recipes.
 
 {recipes}
@@ -52,89 +52,20 @@ DEVELOPER_MESSAGE_TEMPLATE = """You're a private chef named chefbot. The couple 
 oai = OpenAI(max_retries=3, timeout=60)
 
 
-# TODO: Message slack on a schedule with menu for the week.
-# Allow interaction in a thread (bolt app), both about the menu but also on-demand (at any time).
-# Bonus: if there's a really good recipe, open a PR to merge it to the repo.
 def make_prompt(write=False):
-    paths = [
-        'recipes/alkaline-noodles.md',
-        'recipes/almond-cake.md',
-        'recipes/banana-bread.md',
-        'recipes/basque-cheesecake.md',
-        'recipes/black-bean-soup.md',
-        'recipes/blueberry-muffins.md',
-        'recipes/caldo-verde.md',
-        'recipes/candied-walnuts.md',
-        'recipes/chicken-and-dumplings.md',
-        'recipes/chicken-and-pepper-stir-fry.md',
-        'recipes/chicken-noodle-soup.md',
-        'recipes/chicken-salad.md',
-        'recipes/chili-oil.md',
-        'recipes/chocolate-chip-cookies.md',
-        'recipes/chocolate-ice-cream.md',
-        'recipes/chocolate-mug-cake.md',
-        'recipes/churros.md',
-        'recipes/cioppino.md',
-        'recipes/clam-chowder.md',
-        'recipes/daikon-rib-soup.md',
-        'recipes/dashi.md',
-        'recipes/dumplings.md',
-        'recipes/egg-and-tomato.md',
-        'recipes/financiers.md',
-        'recipes/florentines.md',
-        'recipes/garlic-shrimp.md',
-        'recipes/gazpacho.md',
-        'recipes/general-tsos-chicken.md',
-        'recipes/granola.md',
-        'recipes/lemon-bars.md',
-        'recipes/lobster-rolls.md',
-        'recipes/miso-soup.md',
-        'recipes/nachos.md',
-        'recipes/oatmeal-raisin-cookies.md',
-        'recipes/orecchiette-with-sausage-and-broccoli-rabe.md',
-        'recipes/pan-con-tomate.md',
-        'recipes/pan-fried-baozi.md',
-        'recipes/panzanella.md',
-        'recipes/pho-bo.md',
-        'recipes/pie-crust.md',
-        'recipes/pizza.md',
-        'recipes/pizzelles.md',
-        'recipes/pot-beans.md',
-        'recipes/potato-salad.md',
-        'recipes/pumpkin-pie.md',
-        'recipes/ratatouille.md',
-        'recipes/red-cooked-fish.md',
-        'recipes/red-cooked-ribs.md',
-        'recipes/rice-pudding.md',
-        'recipes/roasted-chicken.md',
-        'recipes/roasted-vegetable.md',
-        'recipes/scones.md',
-        'recipes/sheet-pan-brats.md',
-        'recipes/shoyu-ramen.md',
-        'recipes/smashed-cucumber-salad.md',
-        'recipes/soft-boiled-eggs.md',
-        'recipes/soltero.md',
-        'recipes/somen.md',
-        'recipes/soondubu-jjigae.md',
-        'recipes/spaetzle.md',
-        'recipes/sugar-cookies.md',
-        'recipes/taiwanese-beef-noodle-soup.md',
-        'recipes/thumbprint-cookies.md',
-        'recipes/tomato-soup.md',
-        'recipes/wontons-in-chili-oil.md',
-        'recipes/yeasted-waffles.md',
-        'recipes/yuca-fries.md',
-    ]
-
+    recipes = Path('recipes')
     contents = []
-    for path in paths:
-        with open(path) as f:
-            content = f.read()
-            contents.append(content.strip())
 
-    recipes = '\n\n'.join(contents)
-    # NOTE: o3-mini-2025-01-31, medium reasoning effort works well. Markdown isn't always correct.
-    prompt = DEVELOPER_MESSAGE_TEMPLATE.format(recipes=recipes)
+    for file in recipes.iterdir():
+        if file.suffix == '.md':
+            with file.open() as f:
+                content = f.read()
+
+                if content:
+                    contents.append(content.strip())
+
+    joined_recipes = '\n\n'.join(contents)
+    prompt = DEVELOPER_MESSAGE_TEMPLATE.format(recipes=joined_recipes)
 
     if write:
         with open('prompt.txt', 'w') as f:
